f0dcc991169173dc2e24ff74456c8b2e5c844db8
Marco Chiarandini <marco@imada.sdu.dk>
2018-11-27 08:52:21 +0100

    working on sheet5

---
+++ assets/lstsq_eigs.py
@@ -0,0 +1,55 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# Task 1
+def least_squares(A, b):
+    """Calculate the least squares solutions to Ax = b by using the QR
+    decomposition.
+
+    Parameters:
+        A ((m,n) ndarray): A matrix of rank n <= m.
+        b ((m, ) ndarray): A vector of length m.
+
+    Returns:
+        x ((n, ) ndarray): The solution to the normal equations.
+    """
+    raise NotImplementedError("Task 1 Incomplete")
+
+# Task 2
+def line_fit():
+    """Find the least squares line that relates the year to the housing price
+    index for the data in housing.npy. Plot both the data points and the least
+    squares line.
+    """
+    raise NotImplementedError("Task 2 Incomplete")
+
+
+# Task 3
+def polynomial_fit():
+    """Find the least squares polynomials of degree 3, 6, 9, and 12 that relate
+    the year to the housing price index for the data in housing.npy. Plot both
+    the data points and the least squares polynomials in individual subplots.
+    """
+    raise NotImplementedError("Task 3 Incomplete")
+
+
+def plot_ellipse(a, b, c, d, e):
+    """Plot an ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1."""
+    theta = np.linspace(0, 2*np.pi, 200)
+    cos_t, sin_t = np.cos(theta), np.sin(theta)
+    A = a*(cos_t**2) + c*cos_t*sin_t + e*(sin_t**2)
+    B = b*cos_t + d*sin_t
+    r = (-B + np.sqrt(B**2 + 4*A)) / (2*A)
+
+    plt.plot(r*cos_t, r*sin_t)
+    plt.gca().set_aspect("equal", "datalim")
+
+# Task 4
+def ellipse_fit():
+    """Calculate the parameters for the ellipse that best fits the data in
+    ellipse.npy. Plot the original data points and the ellipse together, using
+    plot_ellipse() to plot the ellipse.
+    """
+    raise NotImplementedError("Task 4 Incomplete")
+
