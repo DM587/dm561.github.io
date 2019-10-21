f3987678e3d2d351889cc51c134d2ccab5a3a238
Marco Chiarandini <marco@imada.sdu.dk>
2019-10-21 12:43:03 +0200

    addeed moved material

---
+++ _solutions/sheet5.py
@@ -0,0 +1,7 @@
+import scipy as sc
+from scipy import linalg as la
+import matplotlib
+
+A = sc.array([0,1],[2,1],[2,1])
+b = sc.array([1,1,2]) # numy and scipy can figure out themselves if it has to be a column vector
+
